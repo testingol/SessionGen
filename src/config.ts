2bba3e21a0830b4d975d2c498b702c1811b39a6d
naxor <naxordeve@gmail.com>
2025-03-10 00:56:24 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -3,13 +3,4 @@
  * Pastebin api goes here lol
  */
 export const PASTEBIN_API_KEY = " ";
-
-/**
- * Authentication file path
- */
-export const AUTH_FILE = '../session/creds.json';
-
-/**
- * Toggle between pairing code and QR code (true = pairing code, false = QR code)
- */
-export const USE_PAIRING_CODE = true;
+export const AUTH = '../session/creds.json';
