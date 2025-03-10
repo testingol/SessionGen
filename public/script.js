a2845b5ba0ffed54611d55fb41c3ba7016118cd9
naxor <naxordeve@gmail.com>
2025-03-10 09:37:36 +0200

    Create script.js

---
+++ public/script.js
@@ -0,0 +1,21 @@
+async function Pairing() {
+    const phone_ = document.getElementById("phone").value.trim();
+    const preloader = document.getElementById("preloader");
+    if (!phone_) {
+    alert("Please enter a valid phone number");
+    return;
+    }
+    preloader.style.display = "flex"; 
+    try { const voidi = await fetch(`/pair?code=${phone_}`);
+        const data = await voidi.json();
+        if (voidi.ok) {
+            localStorage.setItem("code", data.code); 
+            window.location.href = "code.html"; 
+        } else {
+        alert(`${data.message || "err"}`);
+        }} catch (error) {
+        console.error(error);
+        alert("servr err_");
+    } finally {
+        preloader.style.display = "none";
+    }}
