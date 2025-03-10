f8031361bfeb16149513eb56f7d34792a3f6c618
naxor <naxordeve@gmail.com>
2025-03-10 12:21:18 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,5 +4,5 @@
  */
 export const paste_api_dev_key = " ";
 export const auth = '../session/creds.json';
-export const api_user_name = "", //paste username 
-export const api_user_password = "", //paste password
+export const api_user_name = ""; //paste username 
+export const api_user_password = ""; //paste password
